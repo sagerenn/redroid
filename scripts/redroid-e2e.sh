ea0a68bde84df8ddf7065e5b924118b4d047d097
Sage <49195270+sagerenn@users.noreply.github.com>
2026-07-10 05:59:30 +0000

    Start Vector app in e2e test

--- scripts/redroid-e2e.sh
+++ scripts/redroid-e2e.sh
@@ -74,6 +74,8 @@ adb -s "$adb_serial" shell id | grep -q 'uid=0'
 
 magisk_pkg='com.topjohnwu.magisk'
 magisk_activity='com.topjohnwu.magisk.ui.MainActivity'
+vector_manager_pkg='org.lsposed.manager'
+vector_manager_activity='org.lsposed.manager.ui.activity.MainActivity'
 yuntai_pkg='com.ctyun.oa'
 yuntai_activity='com.ctg.itrdc.mf.yimu.modules.splash.ui.YunTaiSplashActivity'
 vector_module_id='zygisk_vector'
@@ -83,6 +85,7 @@ adb -s "$adb_serial" shell am start -W -n "$magisk_pkg/$magisk_activity"
 adb -s "$adb_serial" shell dumpsys activity activities | grep -q "$magisk_pkg"
 
 curl -fsSL "$vector_release_url" -o /tmp/vector-module.zip
+unzip -p /tmp/vector-module.zip manager.apk > /tmp/vector-manager.apk
 adb -s "$adb_serial" push /tmp/vector-module.zip /data/local/tmp/vector-module.zip >/dev/null
 
 adb -s "$adb_serial" shell <<'EOF'
@@ -99,6 +102,11 @@ adb -s "$adb_serial" shell /data/adb/magisk/magisk --install-module /data/local/
 adb -s "$adb_serial" shell test -d "/data/adb/modules_update/$vector_module_id"
 adb -s "$adb_serial" shell test -f "/data/adb/modules_update/$vector_module_id/module.prop"
 adb -s "$adb_serial" shell grep -q '^id=zygisk_vector$' "/data/adb/modules_update/$vector_module_id/module.prop"
+
+adb -s "$adb_serial" install -r /tmp/vector-manager.apk
+adb -s "$adb_serial" shell pm path "$vector_manager_pkg" | grep -q '^package:'
+adb -s "$adb_serial" shell am start -W -n "$vector_manager_pkg/$vector_manager_activity"
+adb -s "$adb_serial" shell dumpsys activity activities | grep -q "$vector_manager_pkg"
 
 adb -s "$adb_serial" install -r yuntai.apk
 adb -s "$adb_serial" shell pm path "$yuntai_pkg" | grep -q '^package:'
